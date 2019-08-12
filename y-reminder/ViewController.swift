4e038a1fb44251206a78a8cbc39c876bfd8babce
YAMATO Keiichi <ymtk172@gmail.com>
2019-08-12 23:05:54 +0900

    Initial Commit

---
+++ y-reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  y-reminder
+//
+//  Created by 大和啓一 on 2019/08/12.
+//  Copyright © 2019 yama172. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
